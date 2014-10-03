e0eb3bbd6fb1bd14d73f7d8ef1db86458058606e
Ilya Kravchenko <ikrav@unl.edu>
2014-10-03 16:06:12 -0500

    Minor updates to cmsRun and crab config scripts

--- ElectronNtupler/test/crab_config_electrons_DY.py
+++ ElectronNtupler/test/crab_config_electrons_DY.py
@@ -2,24 +2,24 @@ from WMCore.Configuration import Configuration
 config = Configuration()
 
 config.section_("General")
-config.General.requestName = 'electron_MC_analysis_test_1'
+config.General.requestName = 'electron_MC_analysis_dy_sc2_1'
 config.General.workArea = 'crab_projects'
 
 config.section_("JobType")
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'runEID_on_DY_cfg.py'
+config.JobType.psetName = 'runEID_eventsDY_cfg.py'
 
 config.section_("Data")
-config.Data.inputDataset = '/DYJetsToLL_M-50_13TeV-madgraph-pythia8/sisagir-Spring14dr-PU_S14_POSTLS170_V6AN1-miniAOD706p1-814812ec83fce2f620905d2bb30e9100/USER'
-config.Data.dbsUrl = 'phys03'
+config.Data.inputDataset = '/DYJetsToLL_M-50_13TeV-madgraph-pythia8/Spring14miniaod-PU20bx25_POSTLS170_V5-v1/MINIAODSIM'
+config.Data.dbsUrl = 'global'
 config.Data.splitting = 'FileBased'
 config.Data.unitsPerJob = 10
-config.Data.publication = True
-config.Data.publishDbsUrl = 'phys03'
-config.Data.publishDataName = 'CRAB3_electron_ntuplizer_test_1'
+config.Data.publication = False
+#config.Data.publishDbsUrl = 'phys03'
+#config.Data.publishDataName = 'CRAB3_electron_ntuplizer_test_7'
 config.Data.ignoreLocality = True
 
 config.section_("Site")
 # Limit to US Tier2 sites. It appears to give more reliable performance.
-config.Site.whitelist = ['T2_US_Nebraska','T2_US_MIT','T2_US_UCSD','T2_US_Florida','T2_US_Wisconsin','T2_US_Caltech','T2_US_Purdue']
+config.Site.whitelist = ['T2_US_MIT','T2_US_UCSD','T2_US_Florida','T2_US_Wisconsin','T2_US_Caltech','T2_US_Purdue']
 config.Site.storageSite = 'T2_US_Nebraska'
--- ElectronNtupler/test/runEID_eventsTT_cfg.py
+++ ElectronNtupler/test/runEID_eventsTT_cfg.py
@@ -4,44 +4,18 @@ process = cms.Process("Ntupler")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
-
-process.source = cms.Source("PoolSource",
-    # 
-    # A few files of the TTJets dataset are listed here, for testing
-    #
-    fileNames = cms.untracked.vstring(
-       '/store/mc/Spring14miniaod/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/MINIAODSIM/PU_S14_POSTLS170_V6-v1/00000/00080927-67FD-E311-B049-0025901D4854.root',
-       '/store/mc/Spring14miniaod/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/MINIAODSIM/PU_S14_POSTLS170_V6-v1/00000/0025EC82-33FD-E311-B455-0025901D4982.root',
-       '/store/mc/Spring14miniaod/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/MINIAODSIM/PU_S14_POSTLS170_V6-v1/00000/002F2689-B7FC-E311-94F1-0025901D46E4.root',
-       '/store/mc/Spring14miniaod/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/MINIAODSIM/PU_S14_POSTLS170_V6-v1/00000/00421178-2EFD-E311-ABFC-0025901D4898.root',
-       '/store/mc/Spring14miniaod/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/MINIAODSIM/PU_S14_POSTLS170_V6-v1/00000/00428AE4-85FC-E311-97F0-0025901D40A6.root',
-       '/store/mc/Spring14miniaod/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/MINIAODSIM/PU_S14_POSTLS170_V6-v1/00000/00678215-05FD-E311-BC19-0025901D40B2.root',
-       '/store/mc/Spring14miniaod/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/MINIAODSIM/PU_S14_POSTLS170_V6-v1/00000/006A58D6-CAFC-E311-9ACA-00266CF959BC.root',
-       '/store/mc/Spring14miniaod/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/MINIAODSIM/PU_S14_POSTLS170_V6-v1/00000/007E939E-71FD-E311-B43F-0025901D4898.root',
-       '/store/mc/Spring14miniaod/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/MINIAODSIM/PU_S14_POSTLS170_V6-v1/00000/008EF5AB-EDFC-E311-9E0B-001EC9B48EB8.root',
-       '/store/mc/Spring14miniaod/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/MINIAODSIM/PU_S14_POSTLS170_V6-v1/00000/00B4426C-82FD-E311-A64D-001EC9B48F62.root',
-       '/store/mc/Spring14miniaod/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/MINIAODSIM/PU_S14_POSTLS170_V6-v1/00000/0203A95D-70FD-E311-AB62-001EC9B08091.root',
-       '/store/mc/Spring14miniaod/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/MINIAODSIM/PU_S14_POSTLS170_V6-v1/00000/02367352-89FC-E311-A988-00266CF94E78.root',
-       '/store/mc/Spring14miniaod/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/MINIAODSIM/PU_S14_POSTLS170_V6-v1/00000/028E7CDC-ACFC-E311-8B9D-0025901D40B2.root',
-       '/store/mc/Spring14miniaod/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/MINIAODSIM/PU_S14_POSTLS170_V6-v1/00000/02C09CE1-D7FC-E311-A3E2-0025901D46E4.root',
-       '/store/mc/Spring14miniaod/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/MINIAODSIM/PU_S14_POSTLS170_V6-v1/00000/02F32F18-76FD-E311-A68B-0025901D4982.root',
-       '/store/mc/Spring14miniaod/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/MINIAODSIM/PU_S14_POSTLS170_V6-v1/00000/0426010E-02FD-E311-9412-001EC9B48F26.root',
-       '/store/mc/Spring14miniaod/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/MINIAODSIM/PU_S14_POSTLS170_V6-v1/00000/0450282D-3CFD-E311-A4A2-0025901D4892.root',
-       '/store/mc/Spring14miniaod/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/MINIAODSIM/PU_S14_POSTLS170_V6-v1/00000/046C2787-35FD-E311-B7E6-0025901D48AA.root',
-       '/store/mc/Spring14miniaod/TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola/MINIAODSIM/PU_S14_POSTLS170_V6-v1/00000/048248D5-55FD-E31'
-    )
-)
+process.load("ElectronWork/ElectronNtupler/TTJets_CSA14_sc2")
 
 process.ntupler = cms.EDAnalyzer('ElectronNtuplerEventStructure',
                                  packed = cms.InputTag("packedGenParticles"),
                                  pruned = cms.InputTag("prunedGenParticles"),
                                  vertices = cms.InputTag("offlineSlimmedPrimaryVertices"),
+                                 pileup = cms.InputTag("addPileupInfo"),
                                  electrons = cms.InputTag("slimmedElectrons"),
 )
 
 process.TFileService = cms.Service("TFileService",
-                                   fileName = cms.string('electron_ntuple_events.root')
+                                   fileName = cms.string('output.root')
                                    )
 
 
