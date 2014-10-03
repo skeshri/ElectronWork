6e12ef8de64870090df7603e3028bc4f5e84f50c
Ilya Kravchenko <ikrav@unl.edu>
2014-10-03 16:04:34 -0500

    A new config for cmsRun for GJets samples

---
+++ ElectronNtupler/test/runEID_eventsGJ_cfg.py
@@ -0,0 +1,22 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("Ntupler")
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+
+process.load("ElectronWork/ElectronNtupler/GJet_CSA14_sc2")
+
+process.ntupler = cms.EDAnalyzer('ElectronNtuplerEventStructure',
+                                 packed = cms.InputTag("packedGenParticles"),
+                                 pruned = cms.InputTag("prunedGenParticles"),
+                                 vertices = cms.InputTag("offlineSlimmedPrimaryVertices"),
+                                 pileup = cms.InputTag("addPileupInfo"),
+                                 electrons = cms.InputTag("slimmedElectrons"),
+)
+
+process.TFileService = cms.Service("TFileService",
+                                   fileName = cms.string('output.root')
+                                   )
+
+
+process.p = cms.Path(process.ntupler)
